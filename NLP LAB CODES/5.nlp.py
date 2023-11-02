383add06314fac82d9908b947361538d6a84c68b
Sooriyarajan <114045757+Sooriyarajan@users.noreply.github.com>
2023-11-02 11:37:24 +0530

    Add files via upload

---
+++ NLP LAB CODES/5.nlp.py
@@ -0,0 +1,8 @@
+import nltk
+from nltk.stem import PorterStemmer
+nltk.download('punkt')
+stemmer = PorterStemmer()
+words = ["running", "flies", "jumps", "quickly", "happily", "unhappiness"]
+stemmed_words = [stemmer.stem(word) for word in words]
+for original, stemmed in zip(words, stemmed_words):
+    print(f"{original} -> {stemmed}")
